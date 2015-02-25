2ecde4e3307c520e40588759b846d082f8a1488c
Gordon Fontenot <gordon@thoughtbot.com>
2015-02-25 17:10:29 -0500

    Fix indentation in NSURL decoder

--- ArgoTests/Models/NSURL.swift
+++ ArgoTests/Models/NSURL.swift
@@ -5,9 +5,9 @@ extension NSURL: JSONDecodable {
   public typealias DecodedType = NSURL
 
   public class func decode(j: JSON) -> DecodedType? {
-        switch j {
-        case .String(let url): return NSURL(string: url)
-        default: return .None
-        }
+    switch j {
+    case .String(let url): return NSURL(string: url)
+    default: return .None
     }
+  }
 }
